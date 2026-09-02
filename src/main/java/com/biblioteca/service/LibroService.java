78372a2aa081f37d34a688c010cb24a8d1f15511
walterbaya-maker <walter.baya@davinci.edu.ar>
2026-09-02 17:14:34 -0300

    aaaa

--- src/main/java/com/biblioteca/service/LibroService.java
+++ src/main/java/com/biblioteca/service/LibroService.java
@@ -30,6 +30,21 @@ public class LibroService {
         return libroRepository.save(libro);
     }
 
+    public Libro crearLibro2(String titulo, String autor) {
+
+        if (titulo == null || titulo.isBlank()) {
+            throw new RuntimeException("El título es obligatorio");
+        }
+
+        if (autor == null || autor.isBlank()) {
+            throw new RuntimeException("El autor es obligatorio");
+        }
+
+        Libro libro = new Libro(titulo, autor);
+
+        return libroRepository.save(libro);
+    }
+
     public List<Libro> listarLibros() {
         return libroRepository.findAll();
     }
